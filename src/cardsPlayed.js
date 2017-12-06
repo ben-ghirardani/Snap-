9fe58213575a5fe90859840ba8ccd805e99ba41c
Ben <pretty.fiendish@gmail.com>
2017-12-06 22:40:28 +0000

    cardsPlayed added

---
+++ src/cardsPlayed.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class cardsPlayed extends Component {
+
+    render() {
+        return(
+            <div className="cardsPlayed">
+
+            </div>
+        )
+    } 
+    
+
+}
+
+export default cardsPlayed;
