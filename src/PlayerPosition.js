b68c333a25ef6e09b067e2769f159e2965972451
Ben <pretty.fiendish@gmail.com>
2018-01-07 23:13:16 +0000

    styling

--- src/PlayerPosition.js
+++ src/PlayerPosition.js
@@ -5,16 +5,15 @@ class PlayerPosition extends Component {
     render() {
         return (
             <div className="player-container">
-            Your Deck
-            <br></br>
-            <img className="player-deck-top" src={this.props.playerDeckTop} />
-            <br></br>
-            <div> Cards Remaining: {this.props.playerDeck.length} </div>
-            <button className="playCard" onClick={this.props.playCard}>Play a card</button>
+                Your Deck
+                <br></br>
+                <img className="player-deck-top" src={this.props.playerDeckTop} />
+                <br></br>
+                <div> Cards Remaining: {this.props.playerDeck.length} </div>
+                <button className="playCard" onClick={this.props.playCard}>Play a card</button>
             </div>
         );
     }
-
 }
 
 export default PlayerPosition;
